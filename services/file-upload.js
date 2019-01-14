4c7dd62c1410e268fddffa2025a2220f8e628812
Juan Rozo <rozoacg@gmail.com>
2019-01-14 17:32:55 -0500

    integracion con s3

--- services/file-upload.js
+++ services/file-upload.js
@@ -18,7 +18,7 @@ var upload = multer({
       cb(null, {fieldName: 'EJEMPLO'});
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString())
+      cb(null, 'hola');
     }
   })
 })
