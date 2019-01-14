13cf83fd5d2c06d874b4695f0e3b24b42258a46d
Juan Rozo <rozoacg@gmail.com>
2019-01-14 18:02:35 -0500

    integracion con s3

--- services/file-upload.js
+++ services/file-upload.js
@@ -11,12 +11,10 @@ var s3 = new aws.S3()
  
 var upload = multer({
   storage: multerS3({
-    dest: 'usuarios/',
     s3: s3,
     bucket: 'econtainers',
     acl: 'public-read',
     metadata: function (req, file, cb) {
-      console.log(file);
       cb(null, {fieldName: file.fieldname});
     },
     key: function (req, file, cb) {
