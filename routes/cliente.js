59712a6e2c5bba3902d230f347bd37eee824bfcd
Juan Rozo <rozoacg@gmail.com>
2019-01-18 11:26:37 -0500

    ajustes

--- routes/cliente.js
+++ routes/cliente.js
@@ -4,7 +4,7 @@ var app = express();
 var Cliente = require("../models/cliente");
 var mdAutenticacion = require("../middlewares/autenticacion");
 var moment = require("moment");
-var lead = require("lead");
+var lead = require("/lead");
 
 // =============================
 // OBTENER CLIENTE
