3ae5c77d61d9efb44bff75e43b9b7fbf87b5b502
Juan Rozo <rozoacg@gmail.com>
2019-01-18 15:55:34 -0500

    ultima

--- routes/lead.js
+++ routes/lead.js
@@ -42,11 +42,7 @@ module.exports = {
            .populate('id_segmento')
            .exec((err, comerciales) => {
       if (err) {
-            return res.status(500).json({
-              ok: false,
-              mensaje: "Error cargando usuarios",
-              error: err
-            });
+      console.log('se putio');
           }
 
           console.log(comerciales);
