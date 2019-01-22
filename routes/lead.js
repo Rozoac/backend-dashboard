0b1750398a6bf9fb394d3e02bbfd845e649559e3
Juan Rozo <rozoacg@gmail.com>
2019-01-22 09:34:21 -0500

    AJUSTE

--- routes/lead.js
+++ routes/lead.js
@@ -1,9 +1,36 @@
 var Usuario = require("../models/usuario");
 var Lead = require("../models/lead");
-var moment = require("moment");
 
 
 
+// =============================
+// OBTENER LEAD POR ID
+// =============================
 
+app.get("/", (req, res, next) => {
+  var id = req.params.id;
+  var desde = req.query.desde || 0;
+  desde = Number(desde);
+
+  Cliente.find({'id_usuario':ObjectId(id)})
+  .skip(desde)
+  .limit(20)
+  .exec((err, clientes) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error cargando clientes",
+        error: err
+      });
+    }
+    Cliente.count({}, (err, conteo) => {
+      res.status(200).json({
+        ok: true,
+        clientes,
+        total: conteo
+      });
+    });
+  });
+});
 
   
