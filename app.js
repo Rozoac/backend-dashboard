ade57baf9ab9c516943fbcd7d44681c45c1af65f
Juan Rozo <rozoacg@gmail.com>
2019-01-22 17:27:31 -0500

    agregado sockets.io

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("./config/config");
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 const server = require('http').createServer(express);
 const io = require('socket.io')(server);
 var app = express();
@@ -12,18 +13,20 @@ var app = express();
 /*=================================================
                     CORS
 ==================================================*/
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+//   next();
+// });
+
 
 app.use(bodyParser.urlencoded({ expented: false }));
 app.use(bodyParser.json());
+app.use(cors({origin:true, credentials:true}));
 
 //Importar rutas
   var appRoutes = require("./routes/app");
