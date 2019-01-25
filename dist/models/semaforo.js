5c30f0fccd20a4ab59f6e6a2f83afb44287a997c
Juan Rozo <rozoacg@gmail.com>
2019-01-25 11:39:51 -0500

    ajuste

--- dist/models/semaforo.js
+++ dist/models/semaforo.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 exports.semaforoSchema = new mongoose_1.Schema({
     estado: { type: String, required: [true, "El estado es necesario"] },
-}, { collection: 'semaforos' });
+});
 exports.Semaforo = mongoose_1.model("Semaforo", exports.semaforoSchema);
--- models/semaforo.ts
+++ models/semaforo.ts
@@ -2,7 +2,7 @@ import { Schema, model} from "mongoose";
 
 export var semaforoSchema = new Schema({
   estado: { type: String, required: [true, "El estado es necesario"] },
-}, {collection: 'semaforos'});
+});
 
 export const Semaforo = model("Semaforo", semaforoSchema);
 
