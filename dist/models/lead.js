d66e2eb7c0765e1c6a02aaee2ec5d50e316fbae4
Juan Rozo <rozoacg@gmail.com>
2019-01-28 09:07:03 -0500

    ajuste

--- dist/models/lead.js
+++ dist/models/lead.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 exports.leadSchema = new mongoose_1.Schema({
-    id_usuario: { type: mongoose_1.Schema.Types.ObjectId, ref: 'Usuario' },
+    id_usuario: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User' },
     id_cliente: { type: mongoose_1.Schema.Types.ObjectId, ref: 'Cliente' },
-    id_semaforo: { type: mongoose_1.Schema.Types.ObjectId, default: '5c4b3f304bec0f00172a8dd3', ref: 'Semaforo' },
+    id_semaforo: { type: mongoose_1.Schema.Types.ObjectId, default: '5c4b5744f1848a00177ab148', ref: 'Semaforo' },
     mensaje: { type: String, required: false },
     fecha_creacion: { type: String, required: true },
     hora_creacion: { type: String, required: true },
